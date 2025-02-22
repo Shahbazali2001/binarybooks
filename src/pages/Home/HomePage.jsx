b38bd39662b85d7e56664a9e4f09c27461a418b1
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-02-23 00:38:46 +0500

    Accordion Added in Hero section

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from './components/Hero'
 import FeaturedProducts from './components/FeaturedProducts'
 import Testimonials from './components/Testimonials'
+import Faq from './components/Faq'
 
 
 const  HomePage = () => {
@@ -10,6 +11,7 @@ const  HomePage = () => {
       <Hero />
       <FeaturedProducts />
       <Testimonials />
+      <Faq />
     </main>
   )
 }
