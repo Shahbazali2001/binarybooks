2d11935ec14949b969fa76d639fda337a204bc17
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-03-02 00:20:51 +0500

    Products Page Added

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { HomePage } from '../pages'
+import  ProductsList  from '../pages/Products/ProductsList'
 
 const AllRoutes = () => {
   return (
     <>
         <Routes>
             <Route path='/' element={<HomePage />} />
+            <Route path='/products' element={<ProductsList />} />
             
         </Routes>
     </>
