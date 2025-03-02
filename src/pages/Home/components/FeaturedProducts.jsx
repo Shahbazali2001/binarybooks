e04f4e501b107a71f4c67cce381d947f5a22bc27
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-03-02 11:54:58 +0500

    Featured Products API Added

--- src/pages/Home/components/FeaturedProducts.jsx
+++ src/pages/Home/components/FeaturedProducts.jsx
@@ -1,18 +1,39 @@
-import React from "react";
+import {useState, useEffect} from "react";
 import ProductCard from "../../../components/Elements/ProductCard";
 
 
 const FeaturedProducts = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(()=>{
+    async function featuredProducts() {
+     try{
+
+      const response = await fetch('http://localhost:3000/featured_products');
+      const data = await response.json();
+      setProducts(data);
+      
+     }catch(error){
+       console.error('Error fetching featured products:', error);
+     }
+      
+    }
+    featuredProducts();
+
+  }, []);
+
+
+
   return (
     <section className="my-20">
       <h1 className="text-2xl text-center font-semibold dark:text-slate-100 mb-5 underline underline-offset-8">
         Featured eBooks
       </h1>
-      <div className="flex flex-wrap justify-center lg:flex-row">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-      </div>
+          <div className="flex flex-wrap justify-center lg:flex-row">
+            {products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}   
+          </div>
     </section>
   );
 };
