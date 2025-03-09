8408bbd939d0e25a2f73e1b2fd6dbcf3315b52e9
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-03-10 01:26:30 +0500

    Product Detail Component Added in Routing

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+
 import { Route, Routes } from 'react-router-dom'
 import { HomePage } from '../pages'
 import  ProductsList  from '../pages/Products/ProductsList'
+import ProductDetail from '../pages/ProductDetail'
 
 const AllRoutes = () => {
   return (
@@ -9,6 +10,7 @@ const AllRoutes = () => {
         <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='/products' element={<ProductsList />} />
+            <Route path='/products/:id' element={<ProductDetail />} />
             
         </Routes>
     </>
