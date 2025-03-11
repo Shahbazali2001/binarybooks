22e49ce76a86d1c3e1fc8c7b3d87540a6da704dc
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-03-12 00:18:31 +0500

    Product Detail Page Completed

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import  Rating from "../components/Elements/Rating";
 
 const ProductDetail = () => {
 
+const {id} = useParams();
 const [product, setProduct] = useState({});
 
 useEffect(()=>{
@@ -22,47 +25,41 @@ useEffect(()=>{
 }, [])
 
 
-
+ const {name, overview, long_description, price, poster, rating, best_seller, in_stock, size} = product;
 
 
   return (
     <main>
       <section>
         <h1 className="mt-10 mb-5 text-4xl text-center font-bold text-gray-900 dark:text-slate-200">
-          The Complete Guide to Backend Development
+          {name}
         </h1>
         <p className="mb-5 text-lg text-center text-gray-900 dark:text-slate-200">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error unde
-          quisquam magni vel eligendi nam.
+          {overview}
         </p>
         <div className="flex flex-wrap justify-around">
           <div className="max-w-xl my-3">
-            <img className="rounded" src="" alt="" />
+            <img className="rounded" src={poster} alt={name} />
           </div>
           <div className="max-w-xl my-3">
             <p className="text-3xl font-bold text-gray-900 dark:text-slate-200">
               <span className="mr-1">$</span>
-              <span className="">29</span>
+              <span className="">{price}</span>
             </p>
             <p className="my-3">
-              <span>
-                <i className="text-lg bi bi-star-fill text-yellow-500 mr-1"></i>
-                <i className="text-lg bi bi-star-fill text-yellow-500 mr-1"></i>
-                <i className="text-lg bi bi-star-fill text-yellow-500 mr-1"></i>
-                <i className="text-lg bi bi-star-fill text-yellow-500 mr-1"></i>
-                <i className="text-lg bi bi-star text-yellow-500 mr-1"></i>
-              </span>
+             <Rating rating={rating} />
             </p>
             <p className="my-4 select-none">
-              <span className="font-semibold text-amber-500 border bg-amber-50 rounded-lg px-3 py-1 mr-2">
+              { best_seller && <span className="font-semibold text-amber-500 border bg-amber-50 rounded-lg px-3 py-1 mr-2">
                 BEST SELLER
-              </span>
+              </span> }
+              { in_stock &&
               <span className="font-semibold text-emerald-600	border bg-slate-100 rounded-lg px-3 py-1 mr-2">
                 INSTOCK
-              </span>
-              {/* <span className="font-semibold text-rose-700 border bg-slate-100 rounded-lg px-3 py-1 mr-2">OUT OF STOCK</span> */}
+              </span> }
+              { !in_stock && <span className="font-semibold text-rose-700 border bg-slate-100 rounded-lg px-3 py-1 mr-2">OUT OF STOCK</span> }
               <span className="font-semibold text-blue-500 border bg-slate-100 rounded-lg px-3 py-1 mr-2">
-                5 MB
+                {size} MB
               </span>
             </p>
             <p className="my-3">
@@ -74,15 +71,7 @@ useEffect(()=>{
               {/* <button className={`inline-flex items-center py-2 px-5 text-lg font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800`}  disabled={ product.in_stock ? "" : "disabled" }>Remove Item <i className="ml-1 bi bi-trash3"></i></button> */}
             </p>
             <p className="text-lg text-gray-900 dark:text-slate-200">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta
-              aut, vel ipsum maxime quam quia, quaerat tempore minus odio
-              exercitationem illum et eos, quas ipsa aperiam magnam officiis
-              libero expedita quo voluptas deleniti sit dolore? Praesentium
-              tempora cumque facere consectetur quia, molestiae quam, accusamus
-              eius corrupti laudantium aliquid! Tempore laudantium unde labore
-              voluptates repellat, dignissimos aperiam ad ipsum laborum
-              recusandae voluptatem non dolore. Reiciendis cum quo illum.
-              Dolorem, molestiae corporis.
+              {long_description}
             </p>
           </div>
         </div>
