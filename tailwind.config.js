92b2e97aba924cdc2438e4d56f2c829933564688
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-03-15 00:38:04 +0500

    Colors Extended

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+    colors: {
+        'primary': '#10b981',
+        'secondary': '#64748b',
+        'dark': '#1E293B',
+        'light': '#F3F4F6',
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
+      },
+    },
   },
   plugins: [],
 }
