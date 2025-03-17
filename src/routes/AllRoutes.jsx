9289b5d9ee14bb9003a39767ce6a37102d2404ed
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-03-18 04:41:34 +0500

    Bug Fixes

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -4,13 +4,14 @@ import { HomePage } from '../pages'
 import  ProductsList  from '../pages/Products/ProductsList'
 import ProductDetail from '../pages/ProductDetail'
 
+
 const AllRoutes = () => {
   return (
     <>
         <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='/products' element={<ProductsList />} />
-            <Route path='/products/:id' element={<ProductDetail />} />
+            <Route path='/product/:id' element={<ProductDetail />} />
             
         </Routes>
     </>
