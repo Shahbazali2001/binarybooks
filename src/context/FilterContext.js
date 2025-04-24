b06b3b58b8c2058f100274674038b471c641be59
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-04-25 02:54:32 +0500

    Context Created For Filtering and Sorting Functionalities

---
+++ src/context/FilterContext.js
@@ -0,0 +1,48 @@
+const filterInitialState ={
+    ProductsList: [],
+    onlyInStock: false,
+    bestSellerOnly: false,
+    sortBy: null,
+    ratings: null,
+	
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Example usage of filterInitialState
+console.log(filterInitialState);
